9eab9ad65c8c2ba9af07df0f227f31e90a5e2bcd
ivor pixabaj <ipix97@hotmail.com>
2019-04-05 15:08:14 -0600

    This the same file tracking project except with another location

---
+++ FileTrackSys/Controllers/ProfilesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileTracking.Models;
+
+namespace FileTracking.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProfilesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            string uName = ParseUsername(User.Identity.Name);
+            var currentUserInDb = _context.AdUsers.Include(u=>u.Branches).Single(u => u.Username == uName);
+            return View("Index", currentUserInDb);
+        }
+        public string ParseUsername(string adName)
+        {
+            string newName = "";
+            if (adName.Contains("DEVFINCO"))
+                newName = adName.Remove(0, 9);
+            return newName;
+        }
+    }
+}
