a26f4c3c62d67ab9164c29efdfe84133b19e8cfe
ivor pixabaj <ipix97@hotmail.com>
2019-06-18 16:15:21 -0600

    ummm

---
+++ FileTrackSys/Models/Transfers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FileTracking.Models
+{
+    public class Transfers
+    {
+        //
+        public int Id { get; set; }
+
+        public FileVolumes Volume { get; set; }
+        public int VolumeId { get; set; }
+
+        public AdUser RequesterUser { get; set; }
+        [ForeignKey("RequesterUser")]
+        public int AdUserId { get; set; }
+
+        public AdUser RegistryUser { get; set; }
+    }
+}
